9fdad7004b1791afc29f014a9e8ffd4c6384e000
5ouma <101255979+5ouma@users.noreply.github.com>
2024-01-15 16:56:36 +0900

    test(io): Separate and test `Save()` and `Load()` more
    In addition to it, check the error is the same as expected.

--- utils/api/io_test.go
+++ utils/api/io_test.go
@@ -2,63 +2,149 @@ package api
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 	"path/filepath"
-	"reflect"
 	"testing"
 )
 
-func Test_IO(t *testing.T) {
+func Test_Save(t *testing.T) {
 	tests := []struct {
-		name      string
-		force     bool
-		wantError bool
+		name          string
+		force         bool
+		expectedError error
 	}{
 		{
-			name:      "✅ Force Save with no error",
-			force:     true,
-			wantError: false,
+			name:          "✅ Save correct with force",
+			force:         true,
+			expectedError: nil,
 		},
 		{
-			name:      "🔴 Save with an exist error",
-			force:     false,
-			wantError: true,
+			name:          "🔴 Save failed with an already exists error",
+			force:         false,
+			expectedError: os.ErrExist,
 		},
 	}
 
 	t.Log("🏗️ Prepare LoginItems Structure")
-	previousLoginItems := LoginItems{
+	loginItems := LoginItems{
 		{"App Store", "/System/Applications/App Store.app", false},
 		{"Calculator", "/System/Applications/Calculator.app", false},
 		{"Notes", "/System/Applications/Notes.app", false},
 		{"Photos", "/System/Applications/Photos.app", false},
 	}
-	data, _ := json.Marshal(previousLoginItems)
-	t.Logf("  loginItems: %v", string(data))
+	data, err := json.Marshal(loginItems)
+	if err != nil {
+		t.Fatalf("🚨 %v", err)
+	}
+	t.Log(string(data))
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			t.Log("🖋️ Save Login Items")
-			path := filepath.Join(t.TempDir(), "login_items.json")
+			path := filepath.Join(t.TempDir(), "login_item.json")
 			os.Create(path)
-			if err := previousLoginItems.Save(path, test.force); !test.wantError && err != nil {
-				t.Fatalf("🚨 %v", err)
-			} else if test.wantError && err == nil {
-				t.Fatal("🚨 expected an error")
-			} else if test.wantError {
-				t.Logf(`  "%v"`, err)
-				t.SkipNow()
+			err := loginItems.Save(path, test.force)
+			if test.expectedError == nil && err != nil {
+				t.Errorf("🚨 %v", err)
+			} else if test.expectedError != nil && err == nil {
+				t.Error("🚨 expected and error")
+			} else if !errors.Is(test.expectedError, err) {
+				t.Errorf("🚨 expected: %v", test.expectedError)
+				t.Errorf("  actual: %v", err)
+			}
+		})
+	}
+}
+
+func Test_Load(t *testing.T) {
+	tests := []struct {
+		name          string
+		content       string
+		createFile    bool
+		expectedError error
+	}{
+		{
+			name: "✅ Load correctly",
+			content: `[
+  {
+    "Name": "App Store",
+    "Path": "/System/Applications/App Store.app",
+    "Hidden": false
+  },
+  {
+    "Name": "Calculator",
+    "Path": "/System/Applications/Calculator.app",
+    "Hidden": false
+  },
+  {
+    "Name": "Notes",
+    "Path": "/System/Applications/Notes.app",
+    "Hidden": false
+  },
+  {
+    "Name": "Photos",
+    "Path": "/System/Applications/Photos.app",
+    "Hidden": false
+  }
+]
+`,
+			createFile:    true,
+			expectedError: nil,
+		},
+		{
+			name: "🔴 Load failed with a not exist error",
+			content: `[
+  {
+    "Name": "App Store",
+    "Path": "/System/Applications/App Store.app",
+    "Hidden": false
+  },
+  {
+    "Name": "Calculator",
+    "Path": "/System/Applications/Calculator.app",
+    "Hidden": false
+  },
+  {
+    "Name": "Notes",
+    "Path": "/System/Applications/Notes.app",
+    "Hidden": false
+  },
+  {
+    "Name": "Photos",
+    "Path": "/System/Applications/Photos.app",
+    "Hidden": false
+  }
+]
+`,
+			createFile:    false,
+			expectedError: os.ErrNotExist,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Log("🏗️ Prepare JSON Structure")
+			t.Log(test.content)
+
+			path := filepath.Join(t.TempDir(), "login_item.json")
+			if test.createFile {
+				t.Log("🖋️ Save Login Items")
+				if err := os.WriteFile(path, []byte(test.content), os.ModePerm); err != nil {
+					t.Fatalf("🚨 %v", err)
+				}
 			}
 
 			t.Log("📘 Read the Saved File")
-			var currentLoginItems LoginItems
-			if err := currentLoginItems.Load(path); err != nil {
-				t.Fatalf("🚨 %v", err)
-			}
-			if !reflect.DeepEqual(previousLoginItems, currentLoginItems) {
-				t.Error("🚨 items are not the same")
-				data, _ := json.Marshal(currentLoginItems)
-				t.Fatalf("  currentLoginItems: %v", string(data))
+			var loginItems LoginItems
+			err := loginItems.Load(path)
+			if test.expectedError == nil && err != nil {
+				t.Errorf("🚨 %v", err)
+			} else if test.expectedError != nil && err == nil {
+				t.Error("🚨 expected and error")
+			} else if !errors.Is(test.expectedError, err) {
+				t.Errorf("🚨 expected: %v", test.expectedError)
+				t.Errorf("  actual: %v", err)
 			}
 		})
 	}
